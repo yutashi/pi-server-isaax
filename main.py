caf70cbef85c47eff137d2cacbaa01b8cc6a4a6a
yuta <katayama@xshell.io>
2018-02-12 13:22:56 +0900

    Update main file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 import os
 app = Flask(__name__)
 MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
-SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
 def hello():
