cca2a520a62ab08484f739d6538bde15d5efb596
yuta <katayama@xshell.io>
2018-02-16 11:51:48 +0900

    Update to display more proper message

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
 import os
 app = Flask(__name__)
-MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
+MESSAGE = os.getenv('MESSAGE', None)
 
 @app.route("/")
 def hello():
-    msg = "Your message running on the device: {}".format(MESSAGE)
-    return msg
+    if MESSAGE:
+        msg = "Your message running on the device: {}".format(MESSAGE)
+    else:
+        msg = "MESSAGE env isn't set"
+
+        return msg
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
